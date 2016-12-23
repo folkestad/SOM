bfe248350792de2352757a74e805a1d07f35cd49
Eirik Folkestad <eirik.ek.folkestad@gmail.com>
2016-12-23 23:25:18 +0100

    remove comments in normalize

--- src/entry.js
+++ src/entry.js
@@ -4,12 +4,12 @@ var som = require("./self_organizing_map.js");
 
 var cities = data.get_data();
 var test = [];
-for(var i = 0; i < 100; i++) {
+for(var i = 0; i < 200; i++) {
     test.push([Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1)]);
 }
 
 var normalized_cities = normalizer.normalize(test);
 
-const self_organizing_map = new som.Self_Organizing_Map(normalized_cities.length*2, 0.65, 150, true, true);
+const self_organizing_map = new som.Self_Organizing_Map(normalized_cities.length*2, 0.65, 250, true, true);
 self_organizing_map.train_neurons(normalized_cities);
 
--- src/normalize.js
+++ src/normalize.js
@@ -1,9 +1,5 @@
 function normalize(data) {
-    var normalized_data = []
-    // console.log("data before");
-    // for (var j = 0; j < data.length; j++) {
-    //     console.log((j+1) + " " + data[j][0] + ":" + data[j][1]);
-    // }
+    var normalized_data = [];
     var x_max = data[0][0];
     var x_min = data[0][0];
     var y_max = data[0][1];
@@ -15,75 +11,14 @@ function normalize(data) {
         if (data[i][1] < y_min) { y_min = data[i][1] };
     }
 
-    // console.log(" ");
-    // console.log("x_max "+x_max);
-    // console.log("x_min "+x_min);
-    // console.log("y_max "+y_max);
-    // console.log("y_min "+y_min);
-    // console.log(" ");
-
     for(i=0; i < data.length; i++) {
         normalized_data.push([normalize_point(data[i][0], x_min, x_max), normalize_point(data[i][1], y_min, y_max)]);
     }
-
-    // console.log("data after");
-    // for (var j = 0; j < normalized_data.length; j++) {
-    //     console.log((j+1) + " " + normalized_data[j][0] + ":" + normalized_data[j][1]);
-    // }
     return normalized_data;
 }
 
 function normalize_point(value, min_val, max_val) {
     return (value - min_val) / (max_val - min_val);
 }
-
-// function denormalize_to_screen(data, width, height) {
-//     var denormalized_data = []
-//     console.log("data before");
-//     for (var j = 0; j < data.length; j++) {
-//         console.log((j+1) + " " + data[j][0] + ":" + data[j][1]);
-//     }
-//     var x_max = data[0][0];
-//     var x_min = data[0][0];
-//     var y_max = data[0][1];
-//     var y_min = data[0][1];
-//     for (var i = 0; i < data.length; i++) {
-//         //console.log(data[i]);
-//         //temp_data.push([parseFloat(data[i][0]),parseFloat(data[i][1])]);
-//         if (data[i][0] > x_max) { x_max = data[i][0] };
-//         if (data[i][0] < x_min) { x_min = data[i][0] };
-//         if (data[i][1] > y_max) { y_max = data[i][1] };
-//         if (data[i][1] < y_min) { y_min = data[i][1] };
-//     }
-//     console.log(" ");
-//     console.log("x_max "+x_max);
-//     console.log("x_min "+x_min);
-//     console.log("y_max "+y_max);
-//     console.log("y_min "+y_min);
-//     console.log(" ");
-
-//     for(var i=0; i < data.length; i++) {
-//         var x = denormalize_point_to_screen(data[i][0], x_min, x_max, width);
-//         //console.log(x);
-//         var y = denormalize_point_to_screen(data[i][1], y_min, y_max, height);
-//         //console.log(y);
-//         denormalized_data.push([x, y]);
-//     }
-//     console.log("data after");
-//     for (var j = 0; j < denormalized_data.length; j++) {
-//         console.log((j+1) + " " + denormalized_data[j][0] + ":" + denormalized_data[j][1]);
-//     }
-//     return denormalized_data;
-// }
-
-// function denormalize_point_to_screen(value, min_val, max_val, dim) {
-//     var delta_err = 0.0000001;
-//     return ((max_val - min_val) / (value - min_val));
-// }
-
-// function denormalize_point(value, min_val, max_val, dim) {
-//     return ((max_val - min_val) / (value - min_val));
-
-// }
 
 module.exports = { normalize: normalize }
